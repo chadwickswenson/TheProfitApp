aff146c6be93e42e1c4eb32108b1c358d704603f
Soroush <hatpick@gmail.com>
2013-12-04 13:21:49 -0800

    sliding and shit

--- src/scripts/controllers.js
+++ src/scripts/controllers.js
@@ -1,11 +1,35 @@
 var ctrls = angular.module('DatAppProfit.controllers', []);
 
-ctrls.controller('AppCtrl', ['$scope', '$location', 'loadingService', function($scope, $location, loadingService) {
-     /* $scope.$on('loadingStarted', function(){
-          $(".loading-panel").show();
-      });
+ctrls.controller('AppCtrl', ['$scope', '$location', '$rootScope', 'loadingService', function($scope, $location, $rootScope, loadingService) {
+	/* $scope.$on('loadingStarted', function(){
+		$(".loading-panel").show();
+	});
+	$scope.$on('loadingStopped', function(){
+		$(".loading-panel").hide();
+	});*/
+	var styles = {
+	    // appear from right
+	    front: '.enter-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;   -webkit-transform:translate3d(100%,0,0)  }  .enter-setup.enter-start {   position:absolute;  -webkit-transform:translate3d(0,0,0)}  .leave-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;   -webkit-transform:translate3d(0,0,0)} .leave-setup.leave-start {   position:absolute;  -webkit-transform:translate3d(-100%,0,0) };',
+	    // appear from left
+	    back: '.enter-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all; -webkit-transform:translate3d(-100%,0,0)}  .enter-setup.enter-start {   position:absolute;   -webkit-transform:translate3d(0,0,0) }  .leave-setup {   position:absolute;   -webkit-transition: 0.5s ease-out all;  -webkit-transform:translate3d(0,0,0)} .leave-setup.leave-start {   position:absolute;  -webkit-transform:translate3d(100%,0,0) };'
+	};
 
-      $scope.$on('loadingStopped', function(){
-          $(".loading-panel").hide();
-      });*/
+	$scope.direction = function(dir) {
+		// update the animations classes
+		$rootScope.style = styles[dir];
+	}
+
+	$scope.go = function(path) {
+		$location.path(path);
+	}
+
+	$scope.direction('front');
+}]);
+
+ctrls.controller('HomeCtrl', ['$scope', '$location', '$rootScope', 'loadingService', function($scope, $location, $rootScope, loadingService) {
+
+}]);
+
+ctrls.controller('AddCtrl', ['$scope', '$location', '$rootScope', 'loadingService', function($scope, $location, $rootScope, loadingService) {
+
 }]);
