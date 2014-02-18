7dda2dcf42c0b7a0ddf2d86e3facbebaa2ef44d0
Soroush <hatpick@gmail.com>
2014-02-18 12:26:22 -0800

    chad fix resize problem

--- cordova/platforms/android/assets/www/scripts/directives.js
+++ cordova/platforms/android/assets/www/scripts/directives.js
@@ -146,15 +146,14 @@ var resizeHandler = function(elem){
     $(elem).find('.top-view').css('height', cH);
 }
 
-$(window).resize(function() {
-    resizeHandler(".view");
-});
-
 components.directive('sizeViews', function(){
     return {
         restrict: 'A',
         link: function(scope, elem, attrs){
             resizeHandler(elem);
+            $(window).resize(function() {
+                resizeHandler($(".view"));
+            });
         }
     }
 
